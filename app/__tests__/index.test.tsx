d31fc75f1f492b24dd3cec467eb12fc5761b07bd
Zweihänder <zweidev@zweihander.me>
2020-05-22 17:22:21 -0400

    Add a few more snapshot tests

--- app/__tests__/index.test.tsx
+++ app/__tests__/index.test.tsx
@@ -1,10 +1,38 @@
 import React from 'react';
 import App from '../App';
-import { render, fireEvent, screen } from '@testing-library/react';
+import Battle from '../components/Battle';
+import Card from '../components/Card';
+import Loading from '../components/Loading';
+import Tooltip from '../components/Tooltip';
 
-test('Popular and battle pages match snapshots', () => {
-	const { asFragment } = render(<App />);
-	expect(asFragment()).toMatchSnapshot();
-	fireEvent.click(screen.getByText('Battle'));
-	expect(asFragment()).toMatchSnapshot();
+import { render } from '@testing-library/react';
+
+const ComponentsArray: Array<{
+	component: React.FC<unknown>;
+	attrs: Record<string, unknown>;
+}> = [
+	{ component: App, attrs: {} },
+	{ component: Battle, attrs: {} },
+	{
+		component: Card,
+		attrs: {
+			header: 'test',
+			avatar: 'test',
+			href: 'test',
+			name: 'test',
+			children: <div></div>,
+		},
+	},
+	{ component: Loading, attrs: {} },
+	{ component: Tooltip, attrs: { text: 'test' } },
+];
+
+describe('Snapshots are matched for ', () => {
+	ComponentsArray.forEach((ComponentObject) => {
+		it(`${ComponentObject.component.name} component`, () => {
+			const { component: Comp, attrs } = ComponentObject;
+			const { asFragment } = render(<Comp {...attrs} />);
+			expect(asFragment()).toMatchSnapshot();
+		});
+	});
 });
